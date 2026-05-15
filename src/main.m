937c5d4515de781eb8d988245d0cad9b8c862837
NinaOmejc <nina.omejc@gmail.com>
2026-05-15 11:08:41 +0200

    Small fix

--- src/main.m
+++ src/main.m
@@ -38,21 +38,21 @@ for isub = 1:length(subjects)
 
         % LABEL DETECTIONS
         events_ShB = detect_shallow_breathing(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
-        % events_IrB = detect_irregular_breathing(data, breaths_lungs, breaths_diaph, config);
-        % events_SlB = detect_slow_breathing(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
-        % events_RaB = detect_rapid_breathing(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
-        % events_ReA = detect_respiratory_asynchrony(data, config);
-        % events_Des = detect_desaturation(data, baseline, spo2_feat, config);
-        % events_Apn = detect_apnea(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
-        % events_Sig = detect_sigh(data, breaths_lungs, breaths_diaph, config);
-        
+        events_IrB = detect_irregular_breathing(data, breaths_lungs, breaths_diaph, config);
+        events_SlB = detect_slow_breathing(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
+        events_RaB = detect_rapid_breathing(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
+        events_ReA = detect_respiratory_asynchrony(data, config);
+        events_Des = detect_desaturation(data, baseline, spo2_feat, config);
+        events_Apn = detect_apnea(data, baseline, breaths_lungs, breaths_diaph, spo2_feat, config);
+        events_Sig = detect_sigh(data, breaths_lungs, breaths_diaph, config);
+
         % JOIN EVENTS FOR SUBJECT, CONDITION
-        % sub_events = merge_events({events_ShB, events_IrB, events_SlB, events_RaB, ...
-        %                            events_ReA, events_Des, events_Apn, events_Sig});
-        % sub_events = normalize_event_types_and_meta(sub_events);
-        % 
-        %N = size(data,1); 
-        %label_mask = events_to_time_mask(sub_events, N);
+        sub_events = merge_events({events_ShB, events_IrB, events_SlB, events_RaB, ...
+                                   events_ReA, events_Des, events_Apn, events_Sig});
+        sub_events = normalize_event_types_and_meta(sub_events);
+
+        N = size(data,1); 
+        label_mask = events_to_time_mask(sub_events, N);
          
         if ~isempty(events_IrB)
             disp(['Found a subject with irr breathing. Its ' num2str(config.subject) ' | M ' num2str(config.measure)])
@@ -61,8 +61,8 @@ for isub = 1:length(subjects)
         % SAVE
         results.subject = config.subject;
         results.condition = config.measure;
-        %results.events = sub_events;
-        %results.mask   = label_mask;
+        results.events = sub_events;
+        results.mask   = label_mask;
         results.baseline = baseline;
         results.config = config;
         save(fullfile(config.sub_results_path, config.sub_results_filename), '-struct', 'results');
